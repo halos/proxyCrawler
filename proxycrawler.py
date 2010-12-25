6857ab450596163ceb31c0d657125a5a4ec46277
halos <halos@lambda>
2010-12-25 23:04:47 +0100

    __get_substitutions method implemented

--- proxycrawler.py
+++ proxycrawler.py
@@ -34,9 +34,36 @@ class proxyCrawler:
 		proxies = []
 		
 	def __get_num_pages(self):
+		"""
+		Method to obtain the number of pages
+		
+		Return:
+		
+			(int): Number of pages of proxies
+		"""
 		
 		page = urllib.urlopen('http://www.samair.ru/proxy/proxy-01.htm').read()
 		
 		num_pages = int(re.findall(u'total pages: (\d+)', page)[0])
 		
 		return num_pages
+		
+	def __get_substitutions(self):
+		"""
+		Method to get the letters that substitute the numbers of the ports
+		
+		Return:
+			(dict): Dictionary which key (str) is the letter and the value 
+					(str) is the number
+		
+		"""
+		
+		page = urllib.urlopen('http://www.samair.ru/proxy/proxy-01.htm').read()
+		
+		subs = {}
+		
+		for let, num in re.findall(u'(.)=(\d)', page):
+			
+			subs[let] = num
+			
+		return subs

ca3f667c64411d0bd0d27eedda136fd83d356f7d
halos <halos@lambda>
2010-12-25 20:47:22 +0100

    First commit

---
+++ proxycrawler.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+class proxyCrawler:
+	"""
+	Class to find proxies
+	"""
+	
+	__proxies = []
+	
+	def __init__(self):
+		"""
+		Constructor
+		"""
+		
+		pass
+		
+	def find(self, criteria):
+		"""
+		Methot Function doc
+	
+		Params:
+	
+			criteria(dict): Criteria for searching proxies
+	
+		Return:
+	
+			(list): Proxies list matching given criteria
+		"""
+		
+		proxies = []
+		
